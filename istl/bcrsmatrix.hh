08ef64658d2d40ac0b9a05c56b77a542781804c5
Markus Blatt <mblatt@dune-project.org>
2006-11-25 21:43:29 +0000

    Bugfix:
    In operator= for assigning a BCRSMatrix that was constructed without
    specifying the number of nonzero one has to calculate the number of
    nonzeros before allocating memory.
    
    [[Imported from SVN: r699]]

--- istl/bcrsmatrix.hh
+++ istl/bcrsmatrix.hh
@@ -427,8 +427,15 @@ namespace Dune {
         // free rows
         A::template free<row_type>(r);
 
+      if (Mat.nnz<=0)
+      {
+        nnz = 0;
+        for (size_type i=0; i<Mat.n; i++)
+          nnz += Mat.r[i].getsize();
+      }
+
       // allocate a,j
-      allocate(Mat.n, Mat.m, Mat.nnz, n!=Mat.n);
+      allocate(Mat.n, Mat.m, nnz, n!=Mat.n);
 
       // build window structure
       copyWindowStructure(Mat);
