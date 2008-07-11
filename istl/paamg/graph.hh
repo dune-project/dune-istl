f14e3b57c74eb7a37df1d3759946d8cff29af4ed
Markus Blatt <mblatt@dune-project.org>
2008-07-11 15:25:22 +0000

    Hopefully ixed flyspray issue 410
    [[Imported from SVN: r917]]

--- istl/paamg/graph.hh
+++ istl/paamg/graph.hh
@@ -666,8 +666,8 @@ namespace Dune
        * @param target The target vertex of the edge we search for.
        * @return The edge we found.
        */
-      const EdgeDescriptor& findEdge(const VertexDescriptor& source,
-                                     const VertexDescriptor& target) const;
+      const EdgeDescriptor findEdge(const VertexDescriptor& source,
+                                    const VertexDescriptor& target) const;
       /**
        * @brief Constructor.
        *
@@ -1936,10 +1936,10 @@ namespace Dune
     }
 
     template<class G, class T>
-    inline const typename SubGraph<G,T>::EdgeDescriptor& SubGraph<G,T>::findEdge(const VertexDescriptor & source,
-                                                                                 const VertexDescriptor & target) const
+    inline const typename SubGraph<G,T>::EdgeDescriptor SubGraph<G,T>::findEdge(const VertexDescriptor& source,
+                                                                                const VertexDescriptor& target) const
     {
-      const EdgeDescriptor& edge = std::lower_bound(edges_+start_[source], edges_+end_[source], target);
+      const EdgeDescriptor edge = std::lower_bound(edges_+start_[source], edges_+end_[source], target);
 #ifdef DUNE_ISTL_WITH_CHECKING
       if(edge==edges_+end_[source] || *edge!=target)
         DUNE_THROW(ISTLError, "No such edge found!");
@@ -2296,14 +2296,14 @@ namespace Dune
     inline E& PropertiesGraph<G,V,E,VM,EM>::getEdgeProperties(const VertexDescriptor& source,
                                                               const VertexDescriptor& target)
     {
-      return edgeProperties_[emap_[graph_.findEdge(source,target)]];
+      return getEdgeProperties(graph_.findEdge(source,target));
     }
 
     template<class G, class V, class E, class VM, class EM>
     inline const E& PropertiesGraph<G,V,E,VM,EM>::getEdgeProperties(const VertexDescriptor& source,
                                                                     const VertexDescriptor& target) const
     {
-      return edgeProperties_[emap_[graph_.findEdge(source,target)]];
+      return getEdgeProperties(graph_.findEdge(source,target));
     }
 
     template<class G, class V, class E, class VM, class EM>
