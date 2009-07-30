747fd756fff788e815fedf4702393f7442999799
Markus Blatt <mblatt@dune-project.org>
2009-07-30 08:59:19 +0000

    Bugfixes (Please merge with future bug fix release)
    - Compiles with g++-4,4 now. Error was error: declaration of
      ‘Dune::SuperLUMatrix<Dune::BCRSMatrix<Dune::FieldMatrix<K, n, p>,
      TA> >& m’shadows template parm ‘int m’ 
      Fixes flyspray #581
    
    - Removed wrong assertion. One should not check whether the whole
      matrix is copied if one just copies a part of it. (Blame copy & paste)
    
    [[Imported from SVN: r1053]]

--- istl/supermatrix.hh
+++ istl/supermatrix.hh
@@ -328,7 +328,7 @@ namespace Dune
     typedef typename Matrix::row_type::const_iterator CIter;
     typedef typename Matrix::size_type size_type;
 
-    SuperMatrixInitializer(SuperLUMatrix& m);
+    SuperMatrixInitializer(SuperLUMatrix& lum);
 
     SuperMatrixInitializer();
 
@@ -650,7 +650,6 @@ namespace Dune
     char name[] = {'A',0};
     if(N_<0)
       dPrint_CompCol_Matrix(name,&A);
-    assert(*this==mat);
 #endif
   }
 
