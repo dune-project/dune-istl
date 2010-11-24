2aeb2ab43399b5400084a0920e34a4a795a812f6
Markus Blatt <mblatt@dune-project.org>
2010-11-24 13:31:08 +0000

    Removed define to prevent warning.
    [[Imported from SVN: r1393]]

--- dune/istl/test/scaledidmatrixtest.cc
+++ dune/istl/test/scaledidmatrixtest.cc
@@ -1,7 +1,6 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 #include "config.h"
-#define DUNE_ISTL_WITH_CHECKING
 
 // include this first to see whether it includes all necessary headers itself
 #include <dune/istl/scaledidmatrix.hh>
