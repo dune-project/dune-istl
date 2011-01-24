30c345cf0ba697310c3f62c73cf666f56372d79c
Markus Blatt <mblatt@dune-project.org>
2011-01-24 14:03:12 +0000

    Made sequential version work, too.
    [[Imported from SVN: r1448]]

--- dune/istl/paamg/pinfo.hh
+++ dune/istl/paamg/pinfo.hh
@@ -34,7 +34,7 @@ namespace Dune
         category = SolverCategory::sequential
       };
 
-      const int getSolverCategory () const {
+      const SolverCategory::Category getSolverCategory () const {
         return SolverCategory::sequential;
       }
 
