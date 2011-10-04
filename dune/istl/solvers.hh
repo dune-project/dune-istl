de0e014ad9f6ffe71ce30c4c6224f81319da87b2
Markus Blatt <mblatt@dune-project.org>
2011-10-04 18:27:06 +0000

    Use the initial defect in the stopping criteria of GMRES (not the defect of the already preconditioned rhs).
    [[Imported from SVN: r1502]]

--- dune/istl/solvers.hh
+++ dune/istl/solvers.hh
@@ -1241,9 +1241,7 @@ namespace Dune {
 
       // clear solver statistics
       res.clear();
-
       _M.pre(x,b);
-
       if (_recalc_defect)
       {
         // norm_0 = norm(M^-1 b)
@@ -1259,9 +1257,9 @@ namespace Dune {
       {
         // norm_0 = norm(M^-1 b)
         w = 0.0; _M.apply(w,b); // w = M^-1 b
-        norm_0 = _sp.norm(w);
         // r = _M.solve(b - A * x);
         _A_.applyscaleadd(-1,x, /* => */ b); // b = b - Ax;
+        norm_0 = _sp.norm(b);
         v[0] = 0.0; _M.apply(v[0],b); // r = M^-1 b
         beta = _sp.norm(v[0]);
       }
@@ -1269,7 +1267,6 @@ namespace Dune {
       // avoid division by zero
       if (norm_0 == 0.0)
         norm_0 = 1.0;
-
       norm = norm_old = _sp.norm(v[0]);
 
       // print header
@@ -1280,7 +1277,7 @@ namespace Dune {
         {
           this->printHeader(std::cout);
           this->printOutput(std::cout,0,norm_0);
-          this->printOutput(std::cout,0,norm);
+          this->printOutput(std::cout,0,norm, norm_0);
         }
       }
 
