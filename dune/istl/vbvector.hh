83d77206ed517bc166e3d6d6ab2b5faf08a19e25
Oliver Sander <sander@dune-project.org>
2012-05-22 08:09:03 +0000

    add missing 'const'
    [[Imported from SVN: r1594]]

--- dune/istl/vbvector.hh
+++ dune/istl/vbvector.hh
@@ -637,7 +637,7 @@ namespace Dune {
 
     //! @returns an iterator that is positioned before
     //! the first entry of the vector.
-    Iterator beforeBegin ()
+    Iterator beforeBegin () const
     {
       return Iterator(block,-1);
     }
