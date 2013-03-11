66ef0750c6afaf1558d0b146c39013932b213edd
Markus Blatt <mblatt@dune-project.org>
2013-03-11 11:05:21 +0000

    Added first version of an AMG method that uses less memory bandwidth
    [[Imported from SVN: r1875]]

--- dune/istl/paamg/test/amgtest.cc
+++ dune/istl/paamg/test/amgtest.cc
@@ -103,8 +103,8 @@ void testAMG(int N, int coarsenTarget, int ml)
   //  Criterion;
   typedef Dune::Amg::AggregationCriterion<Dune::Amg::SymmetricMatrixDependency<BCRSMat,Dune::Amg::FirstDiagonal> > CriterionBase;
   typedef Dune::Amg::CoarsenCriterion<CriterionBase> Criterion;
-  typedef Dune::SeqSSOR<BCRSMat,Vector,Vector> Smoother;
-  //typedef Dune::SeqSOR<BCRSMat,Vector,Vector> Smoother;
+  //typedef Dune::SeqSSOR<BCRSMat,Vector,Vector> Smoother;
+  typedef Dune::SeqSOR<BCRSMat,Vector,Vector> Smoother;
   //typedef Dune::SeqJac<BCRSMat,Vector,Vector> Smoother;
   //typedef Dune::SeqOverlappingSchwarz<BCRSMat,Vector,Dune::MultiplicativeSchwarzMode> Smoother;
   //typedef Dune::SeqOverlappingSchwarz<BCRSMat,Vector,Dune::SymmetricMultiplicativeSchwarzMode> Smoother;
