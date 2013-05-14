eada6c8593917cc579c27f9d2586024b5b434a79
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-05-14 14:19:26 +0200

    Spelling

--- dune/istl/schwarz.hh
+++ dune/istl/schwarz.hh
@@ -279,9 +279,9 @@ namespace Dune {
   /**
    * @brief Block parallel preconditioner.
    *
-   * This is essentially a wrapper that take a sequential
-   * preconditoner. In each step the sequential preconditioner
-   * is applied and then all owner data points are updates on
+   * This is essentially a wrapper that takes a sequential
+   * preconditioner. In each step the sequential preconditioner
+   * is applied and then all owner data points are updated on
    * all other processes.
    */
   template<class X, class Y, class C, class T=Preconditioner<X,Y> >
