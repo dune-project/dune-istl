58bb2e3d88299b8e1159856ca647e81eccb6e8f4
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2013-08-26 14:34:45 +0200

    [CUDA] Simple cuda lib creation example
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

---
+++ dune/istl/test/cudatest.cc
@@ -0,0 +1,13 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include "config.h"
+#include <dune/istl/vector/cuda_backend.hh>
+#include <cstdlib>
+
+//using namespace Dune;
+
+int main()
+{
+  foo();
+  return EXIT_SUCCESS;
+}
---
+++ dune/istl/vector/cuda_backend.cu
@@ -0,0 +1,5 @@
+#include <cuda.h>
+
+void foo()
+{
+}
