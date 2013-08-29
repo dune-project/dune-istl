e92206b2275cdc9629fb0b183932b6e95f6d7e56
Steffen Müthing <muething@dune-project.org>
2013-08-29 15:37:27 +0200

    [Vector] WIP version of host vector with TBB and kernel support
    This patch is mostly just a snapshot of the host vector implementation.
    It should mostly work, provided you have all the dependencies (I've tested
    it with GCC 4.8 and TBB 4.1_4).
    
    The implementation uses TBB to multithread algebraic operations. The actual
    operation kernels are implemented in the new Kernel library in dune-common,
    which has several advantages:
    
    - I noticed that inlining of the kernels messes up the auto-vectorizers of both
      GCC and ICC, so the kernels have to be in external functions anyway.
    
    - Having the kernels in dune-common allows a lot more reuse.
    
    - Precompiling the kernels can save on compilation time (and the mechanism in
      dune-common also allows for specializing specific variants of the kernels,
      if someone feels inclined to play around with SIMD intrinsics).
    
    I've tried designing in support for mixed precision, but that is untested as of
    now.
    
    You need to use one of the special allocators from dune/common/kernel/allocator.hh
    because those export some required extra information (alignment and block size).

--- dune/istl/vector/host.hh
+++ dune/istl/vector/host.hh
@@ -4,20 +4,24 @@
 #ifndef DUNE_ISTL_VECTOR_HOST_HH
 #define DUNE_ISTL_VECTOR_HOST_HH
 
-#if 0
-
 #include <cmath>
-#include <complex>
 #include <memory>
 #include <limits>
+#include <type_traits>
+#include <algorithm>
+#include <functional>
+
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
+#include <tbb/cache_aligned_allocator.h>
 
 #include <dune/common/static_assert.hh>
+#include <dune/common/exceptions.hh>
 #include <dune/common/promotiontraits.hh>
 #include <dune/common/dotproduct.hh>
+#include <dune/common/kernel/vec.hh>
 
-#include "istlexception.hh"
-#include "basearray.hh"
-
+#include <dune/common/threads/range.hh>
 
 namespace Dune {
   namespace ISTL {
@@ -25,103 +29,507 @@ namespace Dune {
     template<typename F_, typename A_>
     class Vector
     {
-      private:
-        size_type _size;
 
-      public:
-        typedef F_ Field;
-        typedef F_::DataType DataType;
-        typedef A_ Allocator;
-        typedef A_ allocator_type;
-        typedef A_::size_type size_type;
-        typedef ... iterator;
-        typedef ... const_iterator;
+    public:
+      typedef F_& Field;
+      typedef F_ DataType;
+      typedef F_ value_type;
 
-        Vector() :
-          _size(0)
-        {
-          //TODO
-        }
+      // F_::DataType DataType;
+      typedef F_ DF;
 
-        Vector(size_type size) :
-          _size(size)
-        {
-          //TODO
-        }
+      typedef typename A_::template rebind<F_>::other Allocator;
+      typedef Allocator allocator_type;
+      typedef typename A_::size_type size_type;
+      typedef value_type* iterator;
+      typedef const value_type* const_iterator;
 
-        Vector(const Vector & other) :
-          _size(other._size)
-        {
-          //TODO
-        }
+      static const size_type block_size = Allocator::block_size;
+      static const size_type alignment = Allocator::alignment;;
 
-        Vector(Vector && other) :
-          _size(other._size)
-        {
-          //TODO
-        }
+    private:
 
-        size_type size() const
-        {
-          return _size;
-        }
+      typedef Threads::fixed_block_size_range<size_type> range_type;
 
-        Vector & operator= (const Vector & other)
-        {
-        }
+    public:
 
-        Vector & operator= (Vector && other)
-        {
-        }
+      Vector()
+        : _size(0)
+        , _allocation_size(0)
+        , _data(nullptr)
+        , _grain_size(Allocator::block_size)
+      {}
 
-        Field operator[] (size_type i)
-        {
-          return _data[i]
-        }
+      Vector(size_type size)
+        : _size(0)
+        , _allocation_size(0)
+        , _data(nullptr)
+        , _grain_size(Allocator::block_size)
+      {
+        allocate(size);
+      }
 
-        const Field operator[] (size_type i) const
-        {
-          return _data[i]
-        }
+      Vector(const Vector & other)
+        : _size(0)
+        , _allocation_size(0)
+        , _data(nullptr)
+        , _grain_size(other._grain_size)
+      {
+        allocate(other._size,false);
+        // TODO: check whether compiler optimizes to memcpy for trivially_copyable types
+        std::uninitialized_copy(other._data,other._data+_size,_data);
+      }
 
-        write(indices, values)
-        {
-        }
+      Vector(Vector && other)
+        : _size(other._size)
+        , _allocation_size(other._allocation_size)
+        , _allocator(std::move(other._allocator))
+        , _data(other._data)
+        , _grain_size(other._grain_size)
+      {
+        other._data = nullptr;
+        other._size = 0;
+        other._allocation_size = 0;
+      }
 
-        read(indices, values)
-        {
-        }
+      size_type size() const
+      {
+        return _size;
+      }
 
-        accumulate(indices, values)
-        {
-        }
+      Vector & operator= (const Vector & other)
+      {
+        if (_size == other._size)
+          {
+            std::copy(other._data,other._data+_size,_data);
+            // don't copy allocator because we keep the old memory!
+            _grain_size = other._grain_size;
+            return *this;
+          }
+        else
+          {
+            if (_data)
+              deallocate();
+            _allocator = other._allocator;
+            if (other._size == 0)
+              return *this;
+            allocate(other._size,false);
+            std::uninitialized_copy(other._data,other._data+_size,_data);
+            _grain_size = other._grain_size;
+          }
+        return *this;
+      }
 
-        write(offset, count, values);
-        read(offset, count, values);
-        accumulate(offset, count, values);
+      Vector & operator= (Vector && other)
+      {
+        if (_data)
+          deallocate();
+        _size = other._size;
+        _allocation_size = other._allocation_size;
+        _allocator = std::move(other._allocator);
+        _data = other._data;
+        _grain_size = other._grain_size;
+        other._data = nullptr;
+        other._size = 0;
+        other._allocation_size = 0;
+      }
 
-        iterator begin()
-        const_iterator begin() const
-        iterator end()
-        const_iterator end() const
+      Field operator[] (size_type i)
+      {
+        return _data[i];
+      }
 
-        scale
-        axpy
-        dot
-        normen
+      const Field operator[] (size_type i) const
+      {
+        return _data[i];
+      }
 
-        setSize(size_type size)
-        {
-          if _size != 0 throw exception
-          alloc(size)
-        }
+      template<typename Indices, typename Values>
+      void read(const Indices& indices, Values& values) const
+      {
+        for (size_type i = 0, end = indices.size(); i != end; ++i)
+           values[i] = _data[indices[i]];
+      }
+
+      template<typename Indices, typename Values>
+      void write(const Indices& indices, const Values& values)
+      {
+        for (size_type i = 0, end = indices.size(); i != end; ++i)
+          _data[indices[i]] = values[i];
+      }
+
+      template<typename Indices, typename Values>
+      void accumulate(const Indices& indices, const Values& values)
+      {
+        for (size_type i = 0, end = indices.size(); i != end; ++i)
+          _data[indices[i]] += values[i];
+      }
+
+      template<typename Values>
+      void read(size_type offset, size_type count, Values& values) const
+      {
+        for (size_type i = 0, o = offset; i != count; ++i, ++o)
+          values[i] = _data[o];
+      }
+
+      template<typename Values>
+      void write(size_type offset, size_type count, const Values& values)
+      {
+        for (size_type i = 0, o = offset; i != count; ++i, ++o)
+          _data[o] = values[i];
+      }
+
+      template<typename Values>
+      void accumulate(size_type offset, size_type count, const Values& values)
+      {
+        for (size_type i = 0, o = offset; i != count; ++i, ++o)
+          _data[o] += values[i];
+      }
+
+      iterator begin()
+      {
+        return _data;
+      }
+
+      iterator end()
+      {
+        return _data + _size;
+      }
+
+      const_iterator begin() const
+      {
+        return _data;
+      }
+
+      const_iterator end() const
+      {
+        return _data + _size;
+      }
+
+      template<typename OF, typename OA>
+      typename enable_if<
+        Kernel::allocators_are_interoperable<
+          allocator_type,
+          OA
+          >::value,
+        Vector
+        >::type&
+      operator+=(const Vector<OF,OA>& other)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            Kernel::vec::blocked::add<
+              value_type,
+              OF,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                other._data+r.begin(),
+                r.block_count());
+          });
+        return *this;
+      }
+
+      template<typename OF, typename OA>
+      typename enable_if<
+        Kernel::allocators_are_interoperable<
+          allocator_type,
+          OA
+          >::value,
+        Vector
+        >::type&
+      operator-=(const Vector<OF,OA>& other)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            value_type* __restrict__  a = _data;
+            typename Vector<OF,OA>::value_type* __restrict__  b = other._data;
+            for (size_type i = r.begin(), end = r.end(); i != end; ++i)
+              a[i] -= b[i];
+          });
+        return *this;
+      }
+
+      template<typename OF, typename OA>
+      typename enable_if<
+        Kernel::allocators_are_interoperable<
+          allocator_type,
+          OA
+          >::value,
+        Vector
+        >::type&
+      operator*=(const Vector<OF,OA>& other)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            Kernel::vec::blocked::mul<
+              value_type,
+              OF,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                other._data+r.begin(),
+                r.block_count());
+          });
+        return *this;
+      }
+
+      Vector& operator*=(value_type b)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            Kernel::vec::blocked::scale<
+              value_type,
+              value_type,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                b,
+                r.block_count());
+          });
+        return *this;
+      }
+
+      Vector& operator/=(value_type b)
+      {
+        return this->operator*=(value_type(1.0)/b);
+      }
+
+      Vector& operator+=(value_type b)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            Kernel::vec::blocked::shift<
+              value_type,
+              value_type,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                b,
+                r.block_count());
+          });
+        return *this;
+      }
+
+      Vector& operator-=(value_type b)
+      {
+        return this->operator+=(-b);
+      }
+
+
+      template<typename OF, typename OA>
+      typename enable_if<
+        Kernel::allocators_are_interoperable<
+          allocator_type,
+          OA
+          >::value,
+        Vector
+        >::type&
+      axpy(value_type a, const Vector<OF,OA>& other)
+      {
+        tbb::parallel_for(
+          iteration_range(),
+          [&](const range_type& r)
+          {
+            Dune::Kernel::vec::blocked::axpy<
+              value_type,
+              value_type,
+              OF,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                a,
+                other._data+r.begin(),
+                r.block_count());
+          });
+        return *this;
+      }
+
+      template<typename OF, typename OA>
+      typename enable_if<
+        Kernel::allocators_are_interoperable<
+          allocator_type,
+          OA
+          >::value,
+        typename PromotionTraits<value_type,OF>::PromotedType
+        >::type
+      dot(const Vector<OF,OA>& other) const
+      {
+        typedef typename PromotionTraits<value_type,OF>::PromotedType result_type;
+        return tbb::parallel_reduce(
+          iteration_range(),
+          result_type(0),
+          [&](const range_type& r, result_type result) -> result_type
+          {
+            return Dune::Kernel::vec::blocked::dot<
+              value_type,
+              OF,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                other._data+r.begin(),
+                r.block_count());
+          },
+          std::plus<result_type>());
+      }
+
+      value_type two_norm2() const
+      {
+        return tbb::parallel_reduce(
+          iteration_range(),
+          DF(0),
+          [&](const range_type& r, DF result) -> DF
+          {
+            return Dune::Kernel::vec::blocked::two_norm2<
+              value_type,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                r.block_count());
+          },
+          std::plus<DF>());
+      }
+
+      value_type two_norm() const
+      {
+        return std::sqrt(two_norm2());
+      }
+
+      value_type one_norm() const
+      {
+        return tbb::parallel_reduce(
+          iteration_range(),
+          DF(0),
+          [&](const range_type& r, DF result) -> DF
+          {
+            return Dune::Kernel::vec::blocked::one_norm<
+              value_type,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                r.block_count());
+          },
+          std::plus<DF>());
+      }
+
+      value_type infinity_norm() const
+      {
+        return tbb::parallel_reduce(
+          iteration_range(),
+          DF(0),
+          [&](const range_type& r, DF result) -> DF
+          {
+            return Dune::Kernel::vec::blocked::infinity_norm<
+              value_type,
+              size_type,
+              alignment,
+              block_size>(
+                _data+r.begin(),
+                r.block_count());
+          },
+          [](const value_type& a, const value_type& b) -> DF
+          {
+            return std::max(a,b);
+          });
+      }
+
+      void setSize(size_type size)
+      {
+        if (_data)
+          DUNE_THROW(NotImplemented,"not allowed");
+        allocate(size);
+      }
+
+
+      ~Vector()
+      {
+        deallocate();
+      }
+
+
+    private:
+
+      range_type iteration_range() const
+      {
+        return range_type(0,_size,block_size,_grain_size);
+      }
+
+      void deallocate()
+      {
+        if (_data)
+          {
+            if (!std::is_trivial<value_type>::value)
+              {
+                tbb::parallel_for(
+                  iteration_range(),
+                  [&](const range_type& r)
+                  {
+                    DF* __restrict__  a = _data;
+                    for (size_type i = r.begin(), end = r.end(); i != end; ++i)
+                      _allocator.destroy(a+i);
+                  });
+              }
+            _allocator.deallocate(_data,_allocation_size);
+            _data = nullptr;
+            _size = 0;
+            _allocation_size = 0;
+          }
+      }
+
+      void allocate(size_type size, bool init = true)
+      {
+        if (_data)
+          DUNE_THROW(NotImplemented,"do not do this");
+        // add padding to block size for matrix operations
+        size_type allocation_size = size + block_size - 1 - ((size-1) % block_size);
+        _data = _allocator.allocate(allocation_size);
+        if (!_data)
+          DUNE_THROW(Exception,"could not allocate memory");
+        _allocation_size = allocation_size;
+        _size = size;
+        if (init && !std::is_trivial<value_type>::value)
+          {
+            tbb::parallel_for(
+              iteration_range(),
+              [&](const range_type& r)
+              {
+                DF* __restrict__  a = _data;
+                for (size_type i = r.begin(), end = r.end(); i != end; ++i)
+                  _allocator.construct(a+i);
+              });
+          }
+        // always initialize padded memory area
+        for (size_type i = size, end = allocation_size; i != end; ++i)
+          _allocator.construct(_data+i);
+      }
+
+      size_type _size;
+      size_type _allocation_size;
+      Allocator _allocator;
+      value_type* _data;
+      size_type _grain_size;
 
     };
 
-
-  } // end namespace istl
-} // end namespace dune
-
-#endif // 0
+  } // end namespace ISTL
+} // end namespace Dune
 
 #endif // DUNE_ISTL_VECTOR_HOST_HH
