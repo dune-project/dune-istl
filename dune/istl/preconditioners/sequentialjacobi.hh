8da897099542ffcdc37bc4015557034b391578d0
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2013-11-05 13:42:13 +0100

    [CUDA] dummy preconditioner
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

--- dune/istl/preconditioners/sequentialjacobi.hh
+++ dune/istl/preconditioners/sequentialjacobi.hh
@@ -174,6 +174,95 @@ namespace Dune {
 
   };
 
+  template<typename M, typename X, typename Y>
+  class SequentialJacobi<M,X,Y,Memory::Domain::CUDA>
+    : public Preconditioner<X,Y> {
+  public:
+    //! \brief The matrix type the preconditioner is for.
+    typedef M Matrix;
+    //! \brief The domain type of the preconditioner.
+    typedef X Domain;
+    //! \brief The range type of the preconditioner.
+    typedef Y Range;
+    //! \brief The field type of the preconditioner.
+    typedef typename X::value_type value_type;
+
+    // TODO: Make sure the allocators are compatible
+
+    typedef typename X::value_type domain_value_type;
+    typedef typename Y::value_type range_value_type;
+    typedef typename Matrix::value_type matrix_value_type;
+    typedef typename Matrix::Allocator::size_type size_type;
+    typedef typename Matrix::range_type range_type;
+
+    // define the category
+    enum {
+      //! \brief The category the preconditioner is part of
+      category=SolverCategory::sequential
+    };
+
+    /*! \brief Constructor.
+
+       Constructor gets all parameters to operate the prec.
+       \param A The matrix to operate on.
+       \param n The number of iterations to perform.
+       \param w The relaxation factor.
+     */
+    SequentialJacobi(const M& A, value_type w, size_type iterations = 3)
+      : _A(A)
+      , _v_new(A.rows())
+      , _w(w)
+      , _iterations(iterations)
+    {}
+
+    /*!
+       \brief Prepare the preconditioner.
+
+       \copydoc Preconditioner::pre(X&,Y&)
+     */
+    virtual void pre (X& x, Y& b) {}
+
+
+
+    /*!
+       \brief Apply the preconditioner.
+
+       \copydoc Preconditioner::apply(X&,const Y&)
+     */
+    virtual void apply (X& v, const Y& d)
+    {
+      //int iterations = _iterations > 10 ? _iterations : 1;
+
+      for (size_type i (0) ; i < _iterations; ++i)
+        {
+          for (size_type j(0) ; j < d.size () ; ++j)
+            _v_new(i, value_type(1) / _A(j, j));
+          v.axpy(_w,_v_new);
+        }
+    }
+
+    /*!
+       \brief Clean up.
+
+       \copydoc Preconditioner::post(X&)
+     */
+    virtual void post (X& x) {}
+
+  private:
+
+    //! \brief The matrix we operate on.
+    const M& _A;
+
+    //! Vector for temporary output storage
+    Y _v_new;
+
+    //! \brief The relaxation parameter to use.
+    value_type _w;
+    //! The number of iterations per call to apply()
+    size_type _iterations;
+
+  };
+
   /** @} end documentation */
 
   } // namespace ISTL
