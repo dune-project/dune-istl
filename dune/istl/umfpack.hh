593bec13efc3f4dbdf273d7d38d4a23b99ceb6d5
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-12-04 15:10:36 +0100

    [cleanup] Encode file path in single-inclusion macro

--- dune/istl/umfpack.hh
+++ dune/istl/umfpack.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_UMFPACK_HH
-#define DUNE_UMFPACK_HH
+#ifndef DUNE_ISTL_UMFPACK_HH
+#define DUNE_ISTL_UMFPACK_HH
 
 #if HAVE_UMFPACK
 
@@ -483,4 +483,4 @@ namespace Dune {
 
 #endif //HAVE_UMFPACK
 
-#endif //DUNE_UMFPACK_HH
+#endif //DUNE_ISTL_UMFPACK_HH
