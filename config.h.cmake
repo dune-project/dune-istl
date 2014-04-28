901b595479c2086f1bb3ff97b2bcfdcb72b2bb1c
Markus Blatt <markus@dr-blatt.de>
2014-04-28 17:52:48 +0200

    [cmake] Activate test for METIS and use it where appropriate.
    Just like for the autotools.

--- config.h.cmake
+++ config.h.cmake
@@ -39,6 +39,9 @@
    by configure _and_ if the application uses the PARMETIS_CPPFLAGS */
 #cmakedefine HAVE_PARMETIS ENABLE_PARMETIS
 
+/* Define if you have METIS library */
+#cmakedefine HAVE_METIS 1
+
 /* Define to ENABLE_SUPERLU if the SuperLU library is available */
 #cmakedefine HAVE_SUPERLU ENABLE_SUPERLU
 
