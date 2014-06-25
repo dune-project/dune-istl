1cd1f6381d1412a1ce716c25da53aecc9bfb2101
Ansgar Burchardt <burchardt@igpm.rwth-aachen.de>
2014-06-25 17:42:24 +0200

    [CMake] add_dune_superlu_flags: Add OBJECT parameter
    (cherry picked from commit 0beab99ff18d89a399bcfd83189a0420323fe360)
    Signed-off-by: Christoph Grüninger <gruenich@dune-project.org>

--- cmake/modules/AddSuperLUFlags.cmake
+++ cmake/modules/AddSuperLUFlags.cmake
@@ -7,15 +7,18 @@
 #
 # adds SuperLU flags to the targets for compilation and linking
 #
-function(add_dune_superlu_flags _targets)
+function(add_dune_superlu_flags)
   if(SUPERLU_FOUND)
-    foreach(_target ${_targets})
-      target_link_libraries(${_target} ${SUPERLU_DUNE_LIBRARIES})
+    cmake_parse_arguments(_add_superlu "OBJECT" "" "" ${ARGN})
+    foreach(_target ${_add_superlu_UNPARSED_ARGUMENTS})
+      if(NOT _add_superlu_OBJECT)
+        target_link_libraries(${_target} ${SUPERLU_DUNE_LIBRARIES})
+      endif()
       get_target_property(_props ${_target} COMPILE_FLAGS)
       string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
       set_target_properties(${_target} PROPERTIES COMPILE_FLAGS
         "${_props} ${SUPERLU_DUNE_COMPILE_FLAGS} -DENABLE_SUPERLU=1")
-    endforeach(_target ${_targets})
+    endforeach()
   endif(SUPERLU_FOUND)
 endfunction(add_dune_superlu_flags)
 
