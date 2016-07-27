173d6ea715c663f0159b14988233a45bb5bc0816
Oliver Sander <oliver.sander@tu-dresden.de>
2016-07-27 10:43:36 +0200

    Include missing header <memory> (for std::unique_ptr)

--- dune/istl/matrixredistribute.hh
+++ dune/istl/matrixredistribute.hh
@@ -2,6 +2,7 @@
 // vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_ISTL_MATRIXREDISTRIBUTE_HH
 #define DUNE_ISTL_MATRIXREDISTRIBUTE_HH
+#include <memory>
 #include "repartition.hh"
 #include <dune/common/exceptions.hh>
 #include <dune/common/parallel/indexset.hh>
