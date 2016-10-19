dd4e07ded29986bdaa22d33b384df1beff1156a3
Carsten Gräser <graeser@dune-project.org>
2016-10-19 23:32:16 +0200

    Remove check for constructor/assignment from base class
    These constructors and assignments are all implemented using
    a downcast to derived class and may lead to undefined behaviour.
    This commit removes the checks for these methods in preparation
    for their removal.

--- dune/istl/test/basearraytest.cc
+++ dune/istl/test/basearraytest.cc
@@ -11,9 +11,6 @@ int main()
   base_array<double> v1(10);
   base_array<double> v2 = v1;
 
-  // Test constructor from base_array_unmanaged
-  base_array<double> v3 = *(static_cast<base_array_unmanaged<double>*>(&v1));
-
   v1.resize(20);
 
   v1 = v2;
