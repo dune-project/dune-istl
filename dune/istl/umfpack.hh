50b1efcb03089f6d457823a8162dd4c6a8bdb417
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2016-12-16 15:25:11 +0100

    [umfpack] Make UMFPack work with operation counting
    This is *not* a complete implementation, it will not count the
    operations done within UMFPack, but with this patch, it becomes
    possible to compile and run programs that use UMFPACK with operation
    counting.

--- dune/istl/umfpack.hh
+++ dune/istl/umfpack.hh
@@ -542,6 +542,373 @@ namespace Dune {
   {
     enum { value = true };
   };
+
+#ifdef ENABLE_COUNTER
+
+}
+
+#include <dune/pdelab/common/opcounter.hh>
+
+namespace Dune {
+
+
+  template<typename T, typename A, int n, int m>
+  class UMFPack<BCRSMatrix<FieldMatrix<oc::OpCounter<T>,n,m>,A > >
+      : public InverseOperator<
+          BlockVector<FieldVector<oc::OpCounter<T>,m>,
+                      typename A::template rebind<FieldVector<oc::OpCounter<T>,m> >::other>,
+    BlockVector<FieldVector<oc::OpCounter<T>,n>,
+                            typename A::template rebind<FieldVector<oc::OpCounter<T>,n> >::other> >
+  {
+    public:
+    /** @brief The matrix type. */
+    typedef Dune::BCRSMatrix<FieldMatrix<oc::OpCounter<T>,n,m>,A> Matrix;
+    typedef Dune::BCRSMatrix<FieldMatrix<oc::OpCounter<T>,n,m>,A> matrix_type;
+
+    using BaseMatrix = BCRSMatrix<FieldMatrix<T,n,m>,A>;
+
+    /** @brief The corresponding SuperLU Matrix type.*/
+    typedef Dune::ColCompMatrix<BaseMatrix> UMFPackMatrix;
+    /** @brief Type of an associated initializer class. */
+    typedef ColCompMatrixInitializer<BaseMatrix> MatrixInitializer;
+    /** @brief The type of the domain of the solver. */
+    typedef Dune::BlockVector<
+      FieldVector<oc::OpCounter<T>,m>,
+      typename A::template rebind<FieldVector<oc::OpCounter<T>,m> >::other> domain_type;
+    /** @brief The type of the range of the solver. */
+    typedef Dune::BlockVector<
+      FieldVector<oc::OpCounter<T>,n>,
+      typename A::template rebind<FieldVector<oc::OpCounter<T>,n> >::other> range_type;
+
+    /** @brief Construct a solver object from a BCRSMatrix
+     *
+     * This computes the matrix decomposition, and may take a long time
+     * (and use a lot of memory).
+     *
+     *  @param matrix the matrix to solve for
+     *  @param verbose [0..2] set the verbosity level, defaults to 0
+     */
+    UMFPack(const Matrix& matrix, int verbose=0) : matrixIsLoaded_(false)
+    {
+      //check whether T is a supported type
+      static_assert((std::is_same<T,double>::value) || (std::is_same<T,std::complex<double> >::value),
+                    "Unsupported Type in UMFPack (only double and std::complex<double> supported)");
+      Caller::defaults(UMF_Control);
+      setVerbosity(verbose);
+      setMatrix(matrix);
+    }
+
+    /** @brief Constructor for compatibility with SuperLU standard constructor
+     *
+     * This computes the matrix decomposition, and may take a long time
+     * (and use a lot of memory).
+     *
+     * @param matrix the matrix to solve for
+     * @param verbose [0..2] set the verbosity level, defaults to 0
+     */
+    UMFPack(const Matrix& matrix, int verbose, bool) : matrixIsLoaded_(false)
+    {
+      //check whether T is a supported type
+      static_assert((std::is_same<T,double>::value) || (std::is_same<T,std::complex<double> >::value),
+                    "Unsupported Type in UMFPack (only double and std::complex<double> supported)");
+      Caller::defaults(UMF_Control);
+      setVerbosity(verbose);
+      setMatrix(matrix);
+    }
+
+    /** @brief default constructor
+     */
+    UMFPack() : matrixIsLoaded_(false), verbosity_(0)
+    {
+      //check whether T is a supported type
+      static_assert((std::is_same<T,double>::value) || (std::is_same<T,std::complex<double> >::value),
+                    "Unsupported Type in UMFPack (only double and std::complex<double> supported)");
+      Caller::defaults(UMF_Control);
+    }
+
+    /** @brief Try loading a decomposition from file and do a decomposition if unsuccessful
+     * @param mat_ the matrix to decompose when no decoposition file found
+     * @param file the decomposition file
+     * @param verbose the verbosity level
+     *
+     * Use saveDecomposition(char* file) for manually storing a decomposition. This constructor
+     * will decompose mat_ and store the result to file if no file wasn't found in the first place.
+     * Thus, if you always use this you will only compute the decomposition once (and when you manually
+     * deleted the decomposition file).
+     */
+    UMFPack(const Matrix& mat_, const char* file, int verbose=0)
+    {
+      //check whether T is a supported type
+      static_assert((std::is_same<T,double>::value) || (std::is_same<T,std::complex<double> >::value),
+                    "Unsupported Type in UMFPack (only double and std::complex<double> supported)");
+      Caller::defaults(UMF_Control);
+      setVerbosity(verbose);
+      int errcode = Caller::load_numeric(&UMF_Numeric, const_cast<char*>(file));
+      if ((errcode == UMFPACK_ERROR_out_of_memory) || (errcode == UMFPACK_ERROR_file_IO))
+      {
+        matrixIsLoaded_ = false;
+        setMatrix(mat_);
+        saveDecomposition(file);
+      }
+      else
+      {
+        matrixIsLoaded_ = true;
+        std::cout << "UMFPack decomposition successfully loaded from " << file << std::endl;
+      }
+    }
+
+    /** @brief try loading a decomposition from file
+     * @param file the decomposition file
+     * @param verbose the verbosity level
+     * @throws Dune::Exception When not being able to load the file. Does not need knowledge of the
+     * actual matrix!
+     */
+    UMFPack(const char* file, int verbose=0)
+    {
+      //check whether T is a supported type
+      static_assert((std::is_same<T,double>::value) || (std::is_same<T,std::complex<double> >::value),
+                    "Unsupported Type in UMFPack (only double and std::complex<double> supported)");
+      Caller::defaults(UMF_Control);
+      int errcode = Caller::load_numeric(&UMF_Numeric, const_cast<char*>(file));
+      if (errcode == UMFPACK_ERROR_out_of_memory)
+        DUNE_THROW(Dune::Exception, "ran out of memory while loading UMFPack decomposition");
+      if (errcode == UMFPACK_ERROR_file_IO)
+        DUNE_THROW(Dune::Exception, "IO error while loading UMFPack decomposition");
+      matrixIsLoaded_ = true;
+      std::cout << "UMFPack decomposition successfully loaded from " << file << std::endl;
+      setVerbosity(verbose);
+    }
+
+    virtual ~UMFPack()
+    {
+      if ((umfpackMatrix_.N() + umfpackMatrix_.M() > 0) || matrixIsLoaded_)
+        free();
+    }
+
+    /**
+     *  \copydoc InverseOperator::apply(X&, Y&, InverseOperatorResult&)
+     */
+    virtual void apply(domain_type& x, range_type& b, InverseOperatorResult& res)
+    {
+      if (umfpackMatrix_.N() != b.dim())
+        DUNE_THROW(Dune::ISTLError, "Size of right-hand-side vector b does not match the number of matrix rows!");
+      if (umfpackMatrix_.M() != x.dim())
+        DUNE_THROW(Dune::ISTLError, "Size of solution vector x does not match the number of matrix columns!");
+
+      double UMF_Apply_Info[UMFPACK_INFO];
+      Caller::solve(UMFPACK_A,
+                    umfpackMatrix_.getColStart(),
+                    umfpackMatrix_.getRowIndex(),
+                    umfpackMatrix_.getValues(),
+                    reinterpret_cast<double*>(&x[0]),
+                    reinterpret_cast<double*>(&b[0]),
+                    UMF_Numeric,
+                    UMF_Control,
+                    UMF_Apply_Info);
+
+      //this is a direct solver
+      res.iterations = 1;
+      res.converged = true;
+      res.elapsed = UMF_Apply_Info[UMFPACK_SOLVE_WALLTIME];
+
+      printOnApply(UMF_Apply_Info);
+    }
+
+    /**
+     *  \copydoc InverseOperator::apply(X&,Y&,double,InverseOperatorResult&)
+     */
+    virtual void apply (domain_type& x, range_type& b, double reduction, InverseOperatorResult& res)
+    {
+      DUNE_UNUSED_PARAMETER(reduction);
+      apply(x,b,res);
+    }
+
+    /**
+     * @brief additional apply method with c-arrays in analogy to superlu
+     * @param x solution array
+     * @param b rhs array
+     */
+    void apply(T* x, T* b)
+    {
+      double UMF_Apply_Info[UMFPACK_INFO];
+      Caller::solve(UMFPACK_A,
+                    umfpackMatrix_.getColStart(),
+                    umfpackMatrix_.getRowIndex(),
+                    umfpackMatrix_.getValues(),
+                    x,
+                    b,
+                    UMF_Numeric,
+                    UMF_Control,
+                    UMF_Apply_Info);
+      printOnApply(UMF_Apply_Info);
+    }
+
+    /** @brief Set UMFPack-specific options
+     *
+     * This method allows to set various options that control the UMFPack solver.
+     * More specifically, it allows to set values in the UMF_Control array.
+     * Please see the UMFPack documentation for a list of possible options and values.
+     *
+     * \param option Entry in the UMF_Control array, e.g., UMFPACK_IRSTEP
+     * \param value Corresponding value
+     *
+     * \throws RangeError If nonexisting option was requested
+     */
+    void setOption(unsigned int option, double value)
+    {
+      if (option >= UMFPACK_CONTROL)
+        DUNE_THROW(RangeError, "Requested non-existing UMFPack option");
+
+      UMF_Control[option] = value;
+    }
+
+    /** @brief saves a decomposition to a file
+     * @param file the filename to save to
+     */
+    void saveDecomposition(const char* file)
+    {
+      int errcode = Caller::save_numeric(UMF_Numeric, const_cast<char*>(file));
+      if (errcode != UMFPACK_OK)
+        DUNE_THROW(Dune::Exception,"IO ERROR while trying to save UMFPack decomposition");
+    }
+
+    /** @brief Initialize data from given matrix. */
+    void setMatrix(const Matrix& matrix)
+    {
+      if ((umfpackMatrix_.N() + umfpackMatrix_.M() > 0) || matrixIsLoaded_)
+        free();
+      umfpackMatrix_ = reinterpret_cast<const BaseMatrix&>(matrix);
+      decompose();
+    }
+
+    template<class S>
+    void setSubMatrix(const Matrix& _mat, const S& rowIndexSet)
+    {
+      if ((umfpackMatrix_.N() + umfpackMatrix_.M() > 0) || matrixIsLoaded_)
+        free();
+      umfpackMatrix_.setMatrix(_mat,rowIndexSet);
+      decompose();
+    }
+
+    /** @brief sets the verbosity level for the UMFPack solver
+     * @param v verbosity level
+     * The following levels are implemented:
+     * 0 - only error messages
+     * 1 - a bit of statistics on decomposition and solution
+     * 2 - lots of statistics on decomposition and solution
+     */
+    void setVerbosity(int v)
+    {
+      verbosity_ = v;
+      // set the verbosity level in UMFPack
+      if (verbosity_ == 0)
+        UMF_Control[UMFPACK_PRL] = 1;
+      if (verbosity_ == 1)
+        UMF_Control[UMFPACK_PRL] = 2;
+      if (verbosity_ == 2)
+        UMF_Control[UMFPACK_PRL] = 4;
+    }
+
+    /**
+     * @brief Return the matrix factorization.
+     * @warning It is up to the user to keep consistency.
+     */
+    void* getFactorization()
+    {
+      return UMF_Numeric;
+    }
+
+    /**
+     * @brief Return the column compress matrix from UMFPack.
+     * @warning It is up to the user to keep consistency.
+     */
+    UMFPackMatrix& getInternalMatrix()
+    {
+      return umfpackMatrix_;
+    }
+
+    /**
+     * @brief free allocated space.
+     * @warning later calling apply will result in an error.
+     */
+    void free()
+    {
+      if (!matrixIsLoaded_)
+      {
+        Caller::free_symbolic(&UMF_Symbolic);
+        umfpackMatrix_.free();
+      }
+      Caller::free_numeric(&UMF_Numeric);
+      matrixIsLoaded_ = false;
+    }
+
+    const char* name() { return "UMFPACK"; }
+
+    private:
+    typedef typename Dune::UMFPackMethodChooser<T> Caller;
+
+    template<class M,class X, class TM, class TD, class T1>
+    friend class SeqOverlappingSchwarz;
+    friend struct SeqOverlappingSchwarzAssemblerHelper<UMFPack<Matrix>,true>;
+
+    /** @brief computes the LU Decomposition */
+    void decompose()
+    {
+      double UMF_Decomposition_Info[UMFPACK_INFO];
+      Caller::symbolic(static_cast<int>(umfpackMatrix_.N()),
+                       static_cast<int>(umfpackMatrix_.N()),
+                       umfpackMatrix_.getColStart(),
+                       umfpackMatrix_.getRowIndex(),
+                       reinterpret_cast<double*>(umfpackMatrix_.getValues()),
+                       &UMF_Symbolic,
+                       UMF_Control,
+                       UMF_Decomposition_Info);
+      Caller::numeric(umfpackMatrix_.getColStart(),
+                      umfpackMatrix_.getRowIndex(),
+                      reinterpret_cast<double*>(umfpackMatrix_.getValues()),
+                      UMF_Symbolic,
+                      &UMF_Numeric,
+                      UMF_Control,
+                      UMF_Decomposition_Info);
+      Caller::report_status(UMF_Control,UMF_Decomposition_Info[UMFPACK_STATUS]);
+      if (verbosity_ == 1)
+      {
+        std::cout << "[UMFPack Decomposition]" << std::endl;
+        std::cout << "Wallclock Time taken: " << UMF_Decomposition_Info[UMFPACK_NUMERIC_WALLTIME] << " (CPU Time: " << UMF_Decomposition_Info[UMFPACK_NUMERIC_TIME] << ")" << std::endl;
+        std::cout << "Flops taken: " << UMF_Decomposition_Info[UMFPACK_FLOPS] << std::endl;
+        std::cout << "Peak Memory Usage: " << UMF_Decomposition_Info[UMFPACK_PEAK_MEMORY]*UMF_Decomposition_Info[UMFPACK_SIZE_OF_UNIT] << " bytes" << std::endl;
+        std::cout << "Condition number estimate: " << 1./UMF_Decomposition_Info[UMFPACK_RCOND] << std::endl;
+        std::cout << "Numbers of non-zeroes in decomposition: L: " << UMF_Decomposition_Info[UMFPACK_LNZ] << " U: " << UMF_Decomposition_Info[UMFPACK_UNZ] << std::endl;
+      }
+      if (verbosity_ == 2)
+      {
+        Caller::report_info(UMF_Control,UMF_Decomposition_Info);
+      }
+    }
+
+    void printOnApply(double* UMF_Info)
+    {
+      Caller::report_status(UMF_Control,UMF_Info[UMFPACK_STATUS]);
+      if (verbosity_ > 0)
+      {
+        std::cout << "[UMFPack Solve]" << std::endl;
+        std::cout << "Wallclock Time: " << UMF_Info[UMFPACK_SOLVE_WALLTIME] << " (CPU Time: " << UMF_Info[UMFPACK_SOLVE_TIME] << ")" << std::endl;
+        std::cout << "Flops Taken: " << UMF_Info[UMFPACK_SOLVE_FLOPS] << std::endl;
+        std::cout << "Iterative Refinement steps taken: " << UMF_Info[UMFPACK_IR_TAKEN] << std::endl;
+        std::cout << "Error Estimate: " << UMF_Info[UMFPACK_OMEGA1] << " resp. " << UMF_Info[UMFPACK_OMEGA2] << std::endl;
+      }
+    }
+
+    UMFPackMatrix umfpackMatrix_;
+    bool matrixIsLoaded_;
+    int verbosity_;
+    void *UMF_Symbolic;
+    void *UMF_Numeric;
+    double UMF_Control[UMFPACK_CONTROL];
+  };
+
+#endif
+
 }
 
 #endif // HAVE_SUITESPARSE_UMFPACK
