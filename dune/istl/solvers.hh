1b056b3417989d8b1a0519ab922660e6836f15c6
Christian Engwer <christi@dune-project.org>
2017-03-19 01:18:46 +0100

    [factory] update new constructors to take objects only as shared_ptr

--- dune/istl/solvers.hh
+++ dune/istl/solvers.hh
@@ -925,12 +925,12 @@ namespace Dune {
       _restart(restart)
     {}
 
-    RestartedGMResSolver (LinearOperator<X,Y>& op, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
+    RestartedGMResSolver (std::shared_ptr<LinearOperator<X,Y> > op, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
       IterativeSolver<X,Y>::IterativeSolver(op,prec,configuration),
       _restart(configuration.get<int>("restart"))
     {}
 
-    RestartedGMResSolver (LinearOperator<X,Y>& op, ScalarProduct<X>& sp, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
+    RestartedGMResSolver (std::shared_ptr<LinearOperator<X,Y> > op, std::shared_ptr<ScalarProduct<X> > sp, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
       IterativeSolver<X,Y>::IterativeSolver(op,sp,prec,configuration),
       _restart(configuration.get<int>("restart"))
     {}
@@ -1253,13 +1253,13 @@ namespace Dune {
     {}
 
 
-    GeneralizedPCGSolver (LinearOperator<X,Y>& op, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
-      IterativeSolver<X,Y>::IterativeSolver(op,prec,configuration),
+    GeneralizedPCGSolver (std::shared_ptr<LinearOperator<X,X> > op, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
+      IterativeSolver<X,X>::IterativeSolver(op,prec,configuration),
       _restart(configuration.get<int>("restart"))
     {}
 
-    GeneralizedPCGSolver (LinearOperator<X,Y>& op, ScalarProduct<X>& sp, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
-      IterativeSolver<X,Y>::IterativeSolver(op,sp,prec,configuration),
+    GeneralizedPCGSolver (std::shared_ptr<LinearOperator<X,X> > op, std::shared_ptr<ScalarProduct<X> > sp, std::shared_ptr<Preconditioner<X,X> > prec, const ParameterTree& configuration) :
+      IterativeSolver<X,X>::IterativeSolver(op,sp,prec,configuration),
       _restart(configuration.get<int>("restart"))
     {}
     /*!
