da90011f638ca47402e8cfce7a51385027d702f8
Christian Engwer <christi@dune-project.org>
2017-03-27 22:26:47 +0200

    [amg] reenable category check of MatrixHierarchy

--- dune/istl/paamg/hierarchy.hh
+++ dune/istl/paamg/hierarchy.hh
@@ -662,16 +662,8 @@ namespace Dune
       : matrices_(const_cast<MatrixOperator&>(fineOperator)),
         parallelInformation_(const_cast<ParallelInformation&>(pinfo))
     {
-#warning reenable category checks
-      // static_assert((static_cast<int>(MatrixOperator::category) ==
-      //                  static_cast<int>(SolverCategory::sequential)
-      //                || static_cast<int>(MatrixOperator::category) ==
-      //                  static_cast<int>(SolverCategory::overlapping)
-      //                || static_cast<int>(MatrixOperator::category) ==
-      //                  static_cast<int>(SolverCategory::nonoverlapping)),
-      //               "MatrixOperator must be of category sequential or overlapping or nonoverlapping");
-      // if (static_cast<int>(MatrixOperator::category) != static_cast<int>(pinfo.getSolverCategory()))
-      //   DUNE_THROW(ISTLError, "MatrixOperator and ParallelInformation must belong to the same category!");
+      if (SolverCategory::category(fineOperator) != pinfo.getSolverCategory())
+        DUNE_THROW(ISTLError, "MatrixOperator and ParallelInformation must belong to the same category!");
     }
 
     template<class M, class IS, class A>
