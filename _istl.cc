01231d1f853965ff0f3823b6edf1e3617ee5758a
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-04-30 18:20:06 +0200

    always export BlockVector< FieldVector< 1 > > to Python

--- _istl.cc
+++ _istl.cc
@@ -5,8 +5,10 @@
 #include <dune/common/fmatrix.hh>
 
 #include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/bvector.hh>
 
 #include <dune/corepy/istl/bcrsmatrix.hh>
+#include <dune/corepy/istl/bvector.hh>
 
 #include <dune/corepy/pybind11/pybind11.h>
 
@@ -15,6 +17,13 @@ PYBIND11_PLUGIN(_istl)
   pybind11::module module( "_istl" );
 
   Dune::CorePy::registerBCRSMatrix< Dune::BCRSMatrix< Dune::FieldMatrix< double, 1, 1 > > >( module );
+  Dune::CorePy::registerBlockVector< Dune::BlockVector< Dune::FieldVector< double, 1 > > >( module );
+
+  // export solver category
+  pybind11::enum_< Dune::SolverCategory::Category > solverCategory( module, "SolverCategory" );
+  solverCategory.value( "sequential", Dune::SolverCategory::sequential );
+  solverCategory.value( "nonoverlapping", Dune::SolverCategory::nonoverlapping );
+  solverCategory.value( "overlapping", Dune::SolverCategory::overlapping );
 
   return module.ptr();
 }
