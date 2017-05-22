bf0c59260068dbe87e6a97ed23e27ffa7ac3394e
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2017-05-22 16:42:59 +0200

    Fix typo

--- dune/istl/preconditioners.hh
+++ dune/istl/preconditioners.hh
@@ -1,4 +1,4 @@
-e// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_ISTL_PRECONDITIONERS_HH
 #define DUNE_ISTL_PRECONDITIONERS_HH
