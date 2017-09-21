90dafa8cf031c686b8c8c92a9f35fcd0bf67618d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-09-21 17:36:21 +0200

    replace PYBIND11_PLUGIN by PYBIND11_MODULE

--- _istl.cc
+++ _istl.cc
@@ -12,10 +12,8 @@
 
 #include <dune/corepy/pybind11/pybind11.h>
 
-PYBIND11_PLUGIN(_istl)
+PYBIND11_MODULE( _istl, module )
 {
-  pybind11::module module( "_istl" );
-
   Dune::CorePy::registerBCRSMatrix< Dune::BCRSMatrix< Dune::FieldMatrix< double, 1, 1 > > >( module );
   Dune::CorePy::registerBlockVector< Dune::BlockVector< Dune::FieldVector< double, 1 > > >( module );
 
@@ -24,6 +22,4 @@ PYBIND11_PLUGIN(_istl)
   solverCategory.value( "sequential", Dune::SolverCategory::sequential );
   solverCategory.value( "nonoverlapping", Dune::SolverCategory::nonoverlapping );
   solverCategory.value( "overlapping", Dune::SolverCategory::overlapping );
-
-  return module.ptr();
 }
