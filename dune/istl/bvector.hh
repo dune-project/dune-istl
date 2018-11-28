395f6d2fa9728ad05076c5daffcab70f5d56a0b2
Oliver Sander <oliver.sander@tu-dresden.de>
2018-11-28 20:06:05 +0100

    Implement BCRSMatrix<double>

--- dune/istl/bvector.hh
+++ dune/istl/bvector.hh
@@ -901,7 +901,7 @@ namespace Imp {
     //===== type definitions and constants
 
     //! export the type representing the field
-    typedef typename B::field_type field_type;
+    using field_type = typename Imp::BlockTraits<B>::field_type;
 
     //! export the type representing the components
     typedef B block_type;
@@ -1154,7 +1154,7 @@ namespace Imp {
     //===== type definitions and constants
 
     //! export the type representing the field
-    typedef typename B::field_type field_type;
+    using field_type = typename Imp::BlockTraits<B>::field_type;
 
     //! export the type representing the components
     typedef B block_type;
@@ -1166,10 +1166,7 @@ namespace Imp {
     typedef typename A::size_type size_type;
 
     //! increment block level counter
-    enum {
-      //! The number of block level this vector contains.
-      blocklevel = B::blocklevel+1
-    };
+    static constexpr unsigned int blocklevel = Imp::BlockTraits<B>::blockLevel();
 
     //! make iterators available as types
     typedef typename compressed_block_vector_unmanaged<B,A>::Iterator Iterator;
