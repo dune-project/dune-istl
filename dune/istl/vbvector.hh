1efccb4025c1d5cb6400cef7fe26726e6369344c
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2018-12-07 13:45:25 +0100

    [VariableBlockVector] Fix beforeBegin iterator

--- dune/istl/vbvector.hh
+++ dune/istl/vbvector.hh
@@ -417,7 +417,7 @@ namespace Dune {
 
     //! @returns an iterator that is positioned before
     //! the first entry of the vector.
-    Iterator beforeBegin () const
+    Iterator beforeBegin ()
     {
       return --block.begin();
     }
@@ -448,6 +448,13 @@ namespace Dune {
     ConstIterator beforeEnd() const
     {
       return --block.end();
+    }
+
+    //! @returns an iterator that is positioned before
+    //! the first entry of the vector.
+    ConstIterator beforeBegin () const
+    {
+      return --block.begin();
     }
 
     //! end ConstIterator
