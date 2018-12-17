08d906e150016ba7f77aad2508ee354d2f935ef0
Oliver Sander <oliver.sander@tu-dresden.de>
2018-12-17 20:26:17 +0100

    Implement ILU/ILDL preconditioners for scalar matrix entries

--- dune/istl/bvector.hh
+++ dune/istl/bvector.hh
@@ -54,6 +54,30 @@ namespace Imp {
     {
       return 0;
     }
+
+    // Interpret a scalar as a length-1 vector
+    static FieldVector<B,1>& toVector(B& value)
+    {
+      return *reinterpret_cast<FieldVector<B,1>*>(&value);
+    }
+
+    // Interpret a scalar as a length-1 vector
+    static const FieldVector<B,1>& toVector(const B& value)
+    {
+      return *static_cast<FieldVector<B,1>*>(&value);
+    }
+
+    // Interpret a scalar as a 1x1 matrix
+    static FieldMatrix<B,1,1>& toMatrix(B& value)
+    {
+      return *reinterpret_cast<FieldMatrix<B,1,1>*>(&value);
+    }
+
+    // Interpret a scalar as a 1x1 matrix
+    static const FieldMatrix<B,1,1>& toMatrix(const B& value)
+    {
+      return *reinterpret_cast<const FieldMatrix<B,1,1>*>(&value);
+    }
   };
 
   template <class B>
@@ -65,6 +89,30 @@ namespace Imp {
     static constexpr unsigned int blockLevel()
     {
       return B::blocklevel;
+    }
+
+    // This method does nothing: only its counterpart for scalar values does something
+    static B& toVector(B& value)
+    {
+      return value;
+    }
+
+    // This method does nothing: only its counterpart for scalar values does something
+    static const B& toVector(const B& value)
+    {
+      return value;
+    }
+
+    // This method does nothing: only its counterpart for scalar values does something
+    static B& toMatrix(B& value)
+    {
+      return value;
+    }
+
+    // This method does nothing: only its counterpart for scalar values does something
+    static const B& toMatrix(const B& value)
+    {
+      return value;
     }
   };
 
