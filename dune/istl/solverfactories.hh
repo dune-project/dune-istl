567de2081c8d6e60b767a3400cc9a47b2fc2fea0
Nils-Arne Dreier <n.dreier@uni-muenster.de>
2019-08-09 17:49:50 +0200

    add IterativeSolver s from solvers.hh to SolverRepository

--- dune/istl/solverfactories.hh
+++ dune/istl/solverfactories.hh
@@ -13,10 +13,35 @@ namespace Dune {
     namespace {
 #endif
 
+      DUNE_INLINE_VARIABLE const auto loopsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::LoopSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose);
+        };
+
+      DUNE_INLINE_VARIABLE const auto gradientsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::GradientSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose);
+        };
+
       DUNE_INLINE_VARIABLE const auto cgsolver =
         [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
           using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
-          using Range = typename std::decay_t<decltype(*lin_op)>::range_type;
 
           double reduction = config.get("reduction",1e-10);
           int max_iterations = config.get("max-iterations",1000);
@@ -27,7 +52,101 @@ namespace Dune {
           return std::make_shared<Dune::CGSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose);
         };
 
+      DUNE_INLINE_VARIABLE const auto bicgstabsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
 
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::BiCGSTABSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose);
+        };
+
+      DUNE_INLINE_VARIABLE const auto minressolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::MINRESSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose);
+        };
+
+      DUNE_INLINE_VARIABLE const auto restartedgmressolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int restart = config.get("restart", 10);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::RestartedGMResSolver<Domain>>(lin_op, scalarProduct, prec, reduction, restart, max_iterations, verbose);
+        };
+
+      DUNE_INLINE_VARIABLE const auto restartedflexiblegmressolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int restart = config.get("restart", 10);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::RestartedFlexibleGMResSolver<Domain>>(lin_op, scalarProduct, prec, reduction, restart, max_iterations, verbose);
+        };
+
+      DUNE_INLINE_VARIABLE const auto generalizedpcgsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int restart = config.get("restart", 10);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::GeneralizedPCGSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose, restart);
+        };
+
+      DUNE_INLINE_VARIABLE const auto restartedfcgsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int restart = config.get("restart", 10);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::RestartedFCGSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose, restart);
+        };
+
+      DUNE_INLINE_VARIABLE const auto completefcgsolver =
+        [](auto lin_op, const ParameterTree& config, auto comm, auto prec) {
+          using Domain = typename std::decay_t<decltype(*lin_op)>::domain_type;
+
+          double reduction = config.get("reduction",1e-10);
+          int max_iterations = config.get("max-iterations",1000);
+          int restart = config.get("restart", 10);
+          int verbose = config.get("verbose", 0);
+
+          auto scalarProduct = createScalarProduct<Domain>(comm, lin_op->category());
+
+          return std::make_shared<Dune::CompleteFCGSolver<Domain>>(lin_op, scalarProduct, prec, reduction, max_iterations, verbose, restart);
+        };
 #ifndef __cpp_inline_variables
     }
 #endif
