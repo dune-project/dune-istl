70a87b2d1996ba2d42b751d63d3c3934aa3bd16d
Nils-Arne Dreier <n.dreier@uni-muenster.de>
2019-12-03 16:39:55 +0100

    [solverfactory] add solverrepository.hh
    This is from an first prove-of-concept branch

---
+++ dune/istl/solverrepository.hh
@@ -0,0 +1,161 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_ISTL_SOLVERREPOSITORY_HH
+#define DUNE_ISTL_SOLVERREPOSITORY_HH
+
+#include <unordered_map>
+#include <functional>
+
+#include <dune/common/parametertree.hh>
+
+#include <dune/istl/preconditionerfactories.hh>
+#include <dune/istl/solverfactories.hh>
+#include <dune/istl/paamg/pinfo.hh>
+
+namespace Dune{
+  /**
+   * @addtogroup ISTL_Solver_Repository
+   * @{
+   */
+  template<class Operator>
+  class PreconditionerRepository {
+    using Domain = typename Operator::domain_type;
+    using Range = typename Operator::range_type;
+    using Preconditioner = Dune::Preconditioner<Domain, Range>;
+    using FactoryType = std::function<std::shared_ptr<Preconditioner>(std::shared_ptr<Operator>,
+                                                                      const ParameterTree&)>;
+  public:
+    using RepositoryType = std::unordered_map<std::string, FactoryType>;
+
+    static RepositoryType repositoryInstance(){
+      using Factories = PreconditionerFactories<Operator>;
+      static RepositoryType repository = {
+                                          {"richardson", Factories::richardson()},
+                                          {"inverseoperator2preconditioner", Factories::inverseoperator2preconditioner()},
+                                          {"seqssor", Factories::seqssor()},
+                                          {"seqsor", Factories::seqsor()},
+                                          {"seqgs", Factories::seqgs()},
+                                          {"seqjac", Factories::seqjac()},
+                                          {"seqilu", Factories::seqilu()},
+                                          {"seqildl", Factories::seqildl()},
+                                          {"parssor", Factories::parssor()},
+                                          {"blockpreconditioner", Factories::blockpreconditioner()},
+                                          {"amg", Factories::amg()},
+                                          {"fastamg", Factories::fastamg()},
+                                          {"kamg", Factories::kamg()},
+                                          {"nonoverlappingblockpreconditioner", Factories::nonoverlappingblockpreconditioner()}
+      };
+      return repository;
+    }
+
+    static void add(const std::string& name, FactoryType factory){
+      repositoryInstance().emplace(name, std::move(factory));
+    }
+
+    static std::shared_ptr<Preconditioner> get(std::shared_ptr<Operator> op,
+                                               const ParameterTree& config){
+      std::string type;
+      try{
+        type = config.template get<std::string>("type");
+      }catch(RangeError&){
+        DUNE_THROW(Exception, "SolverRepository: \"type\" is not set in the config");
+      }
+      FactoryType fac;
+      try{
+        fac = repositoryInstance().at(type);
+      }catch(std::out_of_range&){
+        DUNE_THROW(Exception, "Could not find preconditioner \"" << type <<  "\" in PreconditionerRepository");
+      }
+      return fac(op, config);
+    }
+  };
+
+  template<class Operator>
+  class SolverRepository {
+    using Domain = typename Operator::domain_type;
+    using Range = typename Operator::range_type;
+    using Solver = Dune::InverseOperator<Domain,Range>;
+    using Preconditioner = Dune::Preconditioner<Domain, Range>;
+
+    using FactoryType = std::function<std::shared_ptr<Solver>(std::shared_ptr<Operator>,
+                                                              const ParameterTree&,
+                                                              std::shared_ptr<Preconditioner>)>;
+  public:
+    using RepositoryType = std::unordered_map<std::string, FactoryType>;
+
+    static RepositoryType repositoryInstance(){
+      using Factories = SolverFactories<Operator>;
+      static RepositoryType repository = {
+                                          {"loopsolver", Factories::loopsolver()},
+                                          {"gradientsolver", Factories::gradientsolver()},
+                                          {"cgsolver", Factories::cgsolver()},
+                                          {"bicgstabsolver", Factories::bicgstabsolver()},
+                                          {"minressolver", Factories::minressolver()},
+                                          {"restartedgmressolver", Factories::restartedgmressolver()},
+                                          {"restartedflexiblegmressolver", Factories::restartedflexiblegmressolver()},
+                                          {"generalizedpcgsolver", Factories::generalizedpcgsolver()},
+                                          {"restartedfcgsolver", Factories::restartedfcgsolver()},
+                                          {"completefcgsolver", Factories::completefcgsolver()},
+                                          {"umfpack", Factories::umfpack()},
+                                          {"ldl", Factories::ldl()},
+                                          {"spqr", Factories::spqr()},
+                                          {"superlu", Factories::superlu()},
+                                          {"cholmod", Factories::cholmod()}
+      };
+      return repository;
+    }
+
+    static void add(const std::string& name, FactoryType factory){
+      repositoryInstance().emplace(name, std::move(factory));
+    }
+
+    static std::shared_ptr<Solver> get(std::shared_ptr<Operator> op,
+                                       const ParameterTree& config,
+                                       std::shared_ptr<Preconditioner> prec = nullptr){
+      std::string type;
+      try{
+        type = config.template get<std::string>("type");
+      }catch(RangeError&){
+        DUNE_THROW(Exception, "SolverRepository: \"type\" is not set in the config");
+      }
+      FactoryType fac;
+      try{
+        fac = repositoryInstance().at(type);
+      }catch(std::out_of_range&){
+        DUNE_THROW(Exception, "Could not find solver \"" << type <<  "\" in SolverRepository");
+      }
+
+      if (!prec && config.hasSub("preconditioner")){
+        prec = PreconditionerRepository<Operator>::get(op, config.sub("preconditioner"));
+      }
+      return fac(op, config, prec);
+    }
+  };
+
+  /**
+     \brief Instanciates an `InverseOperator` from an Operator and a
+     configuration given in a ParameterTree.
+     \param op Operator
+     \param config `ParamerTree` with configuration
+     \param prec Custom `Preconditioner` (optional). If not given it will be
+     created with the `PreconditionerRepository` and the configuration given in
+     subKey "preconditioner".
+
+   */
+  template<class Operator>
+  std::shared_ptr<InverseOperator<typename Operator::domain_type,
+                                  typename Operator::range_type>> getSolverFromRepository(std::shared_ptr<Operator> op,
+                               const ParameterTree& config,
+                               std::shared_ptr<Preconditioner<typename Operator::domain_type,
+                               typename Operator::range_type>> prec = nullptr){
+    return SolverRepository<Operator>::get(op, config, prec);
+  }
+
+  /**
+ * @}
+ */
+}
+
+
+#endif
---
+++ dune/istl/test/solverrepositorytest.cc
@@ -0,0 +1,110 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include "config.h"
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/parametertree.hh>
+#include <dune/common/parametertreeparser.hh>
+
+#include <dune/istl/bvector.hh>
+#include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/operators.hh>
+#include <dune/istl/solverrepository.hh>
+#include <dune/istl/paamg/test/anisotropic.hh>
+
+#include "laplacian.hh"
+
+using Vector = Dune::BlockVector<Dune::FieldVector<double, 1>>;
+using Matrix = Dune::BCRSMatrix<Dune::FieldMatrix<double, 1, 1>>;
+
+template<class Comm>
+void testSeq(const Dune::ParameterTree& config, Comm c){
+  if(c.rank() == 0){
+    Matrix mat;
+    int N = config.get("N", 10);
+    setupLaplacian(mat, N);
+    Vector x(mat.M()), b(mat.N());
+
+    using Operator = Dune::MatrixAdapter<Matrix, Vector, Vector>;
+    std::shared_ptr<Dune::LinearOperator<Vector, Vector>> op = std::make_shared<Operator>(mat);
+
+    for(std::string test : config.getSubKeys()){
+      Dune::ParameterTree solverConfig = config.sub(test);
+      std::cout << " ============== " << test << " ============== " << std::endl;
+      std::shared_ptr<Dune::InverseOperator<Vector, Vector>> solver = getSolverFromRepository(op, solverConfig);
+      x = 0;
+      b = 1;
+      Dune::InverseOperatorResult res;
+      solver->apply(x,b,res);
+    }
+  }
+}
+
+template<class Comm>
+void testOverlapping(const Dune::ParameterTree& config, Comm c){
+  Matrix mat;
+  int N = config.get("N", 10);
+  typedef Dune::OwnerOverlapCopyCommunication<int> Communication;
+  Communication comm(c);
+  int n;
+  mat = setupAnisotropic2d<Dune::FieldMatrix<double, 1, 1>>(N, comm.indexSet(), comm.communicator(), &n);
+  comm.remoteIndices().template rebuild<false>();
+  Vector x(mat.M()), b(mat.N());
+
+  using Operator = Dune::OverlappingSchwarzOperator<Matrix, Vector, Vector, Communication>;
+  std::shared_ptr<Operator> op = std::make_shared<Operator>(mat, comm);
+
+  for(const std::string& test : config.getSubKeys()){
+    Dune::ParameterTree solverConfig = config.sub(test);
+    if(c.rank() == 0)
+      std::cout << " ============== " << test << " ============== " << std::endl;
+    std::shared_ptr<Dune::InverseOperator<Vector, Vector>> solver = getSolverFromRepository(op, solverConfig);
+    x = 1;
+    b = 0;
+    setBoundary(x, b, N, comm.indexSet());
+    Dune::InverseOperatorResult res;
+    solver->apply(x,b,res);
+  }
+}
+
+template<class Comm>
+void testNonoverlapping(const Dune::ParameterTree& config, Comm c){
+  Matrix mat;
+  int N = config.get("N", 10);
+  typedef Dune::OwnerOverlapCopyCommunication<int> Communication;
+  Communication comm(c, Dune::SolverCategory::nonoverlapping);
+  int n;
+  mat = setupAnisotropic2d<Dune::FieldMatrix<double, 1, 1>>(N, comm.indexSet(), comm.communicator(), &n);
+  comm.remoteIndices().template rebuild<false>();
+  Vector x(mat.M()), b(mat.N());
+
+  using Operator = Dune::NonoverlappingSchwarzOperator<Matrix, Vector, Vector, Communication>;
+  std::shared_ptr<Operator> op = std::make_shared<Operator>(mat, comm);
+
+  for(const std::string& test : config.getSubKeys()){
+    Dune::ParameterTree solverConfig = config.sub(test);
+    if(c.rank() == 0)
+      std::cout << " ============== " << test << " ============== " << std::endl;
+    std::shared_ptr<Dune::InverseOperator<Vector, Vector>> solver = getSolverFromRepository(op, solverConfig);
+    x = 1;
+    b = 0;
+    setBoundary(x, b, N, comm.indexSet());
+    Dune::InverseOperatorResult res;
+    solver->apply(x,b,res);
+  }
+}
+
+int main(int argc, char** argv){
+  auto& mpihelper = Dune::MPIHelper::instance(argc, argv);
+  Dune::ParameterTree config;
+  Dune::ParameterTreeParser::readINITree("solverrepositorytest.ini", config);
+  Dune::ParameterTreeParser::readOptions(argc, argv, config);
+
+  std::cout << std::endl << " Testing sequential tests... " << std::endl;
+  testSeq(config.sub("sequential"), mpihelper.getCollectiveCommunication());
+  std::cout << std::endl << " Testing overlapping tests... " << std::endl;
+  testOverlapping(config.sub("overlapping"), mpihelper.getCollectiveCommunication());
+    std::cout << std::endl << " Testing nonoverlapping tests... " << std::endl;
+  testNonoverlapping(config.sub("nonoverlapping"), mpihelper.getCollectiveCommunication());
+  return 0;
+}
