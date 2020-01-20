fdc88c7860d981f91599c364ccb37a21763a923c
Markus Blatt <markus@dr-blatt.de>
2020-01-20 14:48:49 +0100

    Remove unused typdef in AMGCreator.

--- dune/istl/paamg/amg.hh
+++ dune/istl/paamg/amg.hh
@@ -1243,7 +1243,6 @@ namespace Dune
     {
       using D = typename Dune::TypeListElement<1, decltype(tl)>::type;
       using R = typename Dune::TypeListElement<2, decltype(tl)>::type;
-      using M = typename OP::matrix_type;
       std::shared_ptr<Preconditioner<D,R>> amg;
       std::string smoother = config.get("smoother", "ssor");
       return makeAMG(op, smoother, config);
