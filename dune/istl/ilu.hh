43d72c165b6bc033257797a94cad27d4da4ceda7
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-01-21 10:28:51 +0100

    [ilu] Use camelCase, move to ILU namespace. Deprecate old names.

--- dune/istl/ilu.hh
+++ dune/istl/ilu.hh
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include <dune/common/fmatrix.hh>
+#include <dune/common/deprecated.hh>
 #include <dune/common/scalarvectorview.hh>
 #include <dune/common/scalarmatrixview.hh>
 #include "istlexception.hh"
@@ -28,235 +29,232 @@ namespace Dune {
     int r, c;
   };
 
+  namespace ILU {
 
-  //! compute ILU decomposition of A. A is overwritten by its decomposition
-  template<class M>
-  void bilu0_decomposition (M& A)
-  {
-    // iterator types
-    typedef typename M::RowIterator rowiterator;
-    typedef typename M::ColIterator coliterator;
-    typedef typename M::block_type block;
-
-    // implement left looking variant with stored inverse
-    rowiterator endi=A.end();
-    for (rowiterator i=A.begin(); i!=endi; ++i)
+    //! compute ILU decomposition of A. A is overwritten by its decomposition
+    template<class M>
+    void blockILU0Decomposition (M& A)
     {
-      // coliterator is diagonal after the following loop
-      coliterator endij=(*i).end();           // end of row i
-      coliterator ij;
+      // iterator types
+      typedef typename M::RowIterator rowiterator;
+      typedef typename M::ColIterator coliterator;
+      typedef typename M::block_type block;
 
-      // eliminate entries left of diagonal; store L factor
-      for (ij=(*i).begin(); ij.index()<i.index(); ++ij)
+      // implement left looking variant with stored inverse
+      rowiterator endi=A.end();
+      for (rowiterator i=A.begin(); i!=endi; ++i)
       {
-        // find A_jj which eliminates A_ij
-        coliterator jj = A[ij.index()].find(ij.index());
+        // coliterator is diagonal after the following loop
+        coliterator endij=(*i).end();           // end of row i
+        coliterator ij;
 
-        // compute L_ij = A_jj^-1 * A_ij
-        Impl::asMatrix(*ij).rightmultiply(Impl::asMatrix(*jj));
-
-        // modify row
-        coliterator endjk=A[ij.index()].end();                 // end of row j
-        coliterator jk=jj; ++jk;
-        coliterator ik=ij; ++ik;
-        while (ik!=endij && jk!=endjk)
-          if (ik.index()==jk.index())
-          {
-            block B(*jk);
-            Impl::asMatrix(B).leftmultiply(Impl::asMatrix(*ij));
-            *ik -= B;
-            ++ik; ++jk;
-          }
-          else
-          {
-            if (ik.index()<jk.index())
-              ++ik;
-            else
-              ++jk;
-          }
-      }
-
-      // invert pivot and store it in A
-      if (ij.index()!=i.index())
-        DUNE_THROW(ISTLError,"diagonal entry missing");
-      try {
-        Impl::asMatrix(*ij).invert();   // compute inverse of diagonal block
-      }
-      catch (Dune::FMatrixError & e) {
-        DUNE_THROW(MatrixBlockError, "ILU failed to invert matrix block A["
-                   << i.index() << "][" << ij.index() << "]" << e.what();
-                   th__ex.r=i.index(); th__ex.c=ij.index(););
-      }
-    }
-  }
-
-  //! LU backsolve with stored inverse
-  template<class M, class X, class Y>
-  void bilu_backsolve (const M& A, X& v, const Y& d)
-  {
-    // iterator types
-    typedef typename M::ConstRowIterator rowiterator;
-    typedef typename M::ConstColIterator coliterator;
-    typedef typename Y::block_type dblock;
-    typedef typename X::block_type vblock;
-
-    // lower triangular solve
-    rowiterator endi=A.end();
-    for (rowiterator i=A.begin(); i!=endi; ++i)
-    {
-      // We need to be careful here: Directly using
-      // auto rhs = Impl::asVector(d[ i.index() ]);
-      // is not OK in case this is a proxy. Hence
-      // we first have to copy the value. Notice that
-      // this is still not OK, if the vector type itself returns
-      // proxy references.
-      dblock rhsValue(d[i.index()]);
-      auto&& rhs = Impl::asVector(rhsValue);
-      for (coliterator j=(*i).begin(); j.index()<i.index(); ++j)
-        Impl::asMatrix(*j).mmv(Impl::asVector(v[j.index()]),rhs);
-      Impl::asVector(v[i.index()]) = rhs;           // Lii = I
-    }
-
-    // upper triangular solve
-    rowiterator rendi=A.beforeBegin();
-    for (rowiterator i=A.beforeEnd(); i!=rendi; --i)
-    {
-      // We need to be careful here: Directly using
-      // auto rhs = Impl::asVector(v[ i.index() ]);
-      // is not OK in case this is a proxy. Hence
-      // we first have to copy the value. Notice that
-      // this is still not OK, if the vector type itself returns
-      // proxy references.
-      vblock rhsValue(v[i.index()]);
-      auto&& rhs = Impl::asVector(rhsValue);
-      coliterator j;
-      for (j=(*i).beforeEnd(); j.index()>i.index(); --j)
-        Impl::asMatrix(*j).mmv(Impl::asVector(v[j.index()]),rhs);
-      auto&& vi = Impl::asVector(v[i.index()]);
-      Impl::asMatrix(*j).mv(rhs,vi);           // diagonal stores inverse!
-    }
-  }
-
-
-
-  // recursive function template to access first entry of a matrix
-  template<class M>
-  typename M::field_type& firstmatrixelement (M& A, typename std::enable_if_t<!Dune::IsNumber<M>::value>* sfinae = nullptr)
-  {
-    return firstmatrixelement(*(A.begin()->begin()));
-  }
-
-  template<class K>
-  K& firstmatrixelement (K& A, typename std::enable_if_t<Dune::IsNumber<K>::value>* sfinae = nullptr)
-  {
-    return A;
-  }
-
-  template<class K, int n, int m>
-  K& firstmatrixelement (FieldMatrix<K,n,m>& A)
-  {
-    return A[0][0];
-  }
-
-
-  /*! ILU decomposition of order n
-          Computes ILU decomposition of order n. The matrix ILU should
-      be an empty matrix in row_wise creation mode. This allows the user
-      to either specify the number of nonzero elements or to
-          determine it automatically at run-time.
-   */
-  template<class M>
-  void bilu_decomposition (const M& A, int n, M& ILU)
-  {
-    // iterator types
-    typedef typename M::ColIterator coliterator;
-    typedef typename M::ConstRowIterator crowiterator;
-    typedef typename M::ConstColIterator ccoliterator;
-    typedef typename M::CreateIterator createiterator;
-    typedef typename M::field_type K;
-    typedef std::map<size_t, int> map;
-    typedef typename map::iterator mapiterator;
-
-    // symbolic factorization phase, store generation number in first matrix element
-    crowiterator endi=A.end();
-    createiterator ci=ILU.createbegin();
-    for (crowiterator i=A.begin(); i!=endi; ++i)
-    {
-      map rowpattern; // maps column index to generation
-
-      // initialize pattern with row of A
-      for (ccoliterator j=(*i).begin(); j!=(*i).end(); ++j)
-        rowpattern[j.index()] = 0;
-
-      // eliminate entries in row which are to the left of the diagonal
-      for (mapiterator ik=rowpattern.begin(); (*ik).first<i.index(); ++ik)
-      {
-        if ((*ik).second<n)
+        // eliminate entries left of diagonal; store L factor
+        for (ij=(*i).begin(); ij.index()<i.index(); ++ij)
         {
-          coliterator endk = ILU[(*ik).first].end();                       // end of row k
-          coliterator kj = ILU[(*ik).first].find((*ik).first);                       // diagonal in k
-          for (++kj; kj!=endk; ++kj)                       // row k eliminates in row i
-          {
-            // we misuse the storage to store an int. If the field_type is std::complex, we have to access the real/abs part
-            // starting from C++11, we can use std::abs to always return a real value, even if it is double/float
-            using std::abs;
-            int generation = (int) Simd::lane(0,abs( firstmatrixelement(*kj) ));
-            if (generation<n)
+          // find A_jj which eliminates A_ij
+          coliterator jj = A[ij.index()].find(ij.index());
+
+          // compute L_ij = A_jj^-1 * A_ij
+          Impl::asMatrix(*ij).rightmultiply(Impl::asMatrix(*jj));
+
+          // modify row
+          coliterator endjk=A[ij.index()].end();                 // end of row j
+          coliterator jk=jj; ++jk;
+          coliterator ik=ij; ++ik;
+          while (ik!=endij && jk!=endjk)
+            if (ik.index()==jk.index())
             {
-              mapiterator ij = rowpattern.find(kj.index());
-              if (ij==rowpattern.end())
+              block B(*jk);
+              Impl::asMatrix(B).leftmultiply(Impl::asMatrix(*ij));
+              *ik -= B;
+              ++ik; ++jk;
+            }
+            else
+            {
+              if (ik.index()<jk.index())
+                ++ik;
+              else
+                ++jk;
+            }
+        }
+
+        // invert pivot and store it in A
+        if (ij.index()!=i.index())
+          DUNE_THROW(ISTLError,"diagonal entry missing");
+        try {
+          Impl::asMatrix(*ij).invert();   // compute inverse of diagonal block
+        }
+        catch (Dune::FMatrixError & e) {
+          DUNE_THROW(MatrixBlockError, "ILU failed to invert matrix block A["
+                     << i.index() << "][" << ij.index() << "]" << e.what();
+                     th__ex.r=i.index(); th__ex.c=ij.index(););
+        }
+      }
+    }
+
+    //! LU backsolve with stored inverse
+    template<class M, class X, class Y>
+    void blockILUBacksolve (const M& A, X& v, const Y& d)
+    {
+      // iterator types
+      typedef typename M::ConstRowIterator rowiterator;
+      typedef typename M::ConstColIterator coliterator;
+      typedef typename Y::block_type dblock;
+      typedef typename X::block_type vblock;
+
+      // lower triangular solve
+      rowiterator endi=A.end();
+      for (rowiterator i=A.begin(); i!=endi; ++i)
+      {
+        // We need to be careful here: Directly using
+        // auto rhs = Impl::asVector(d[ i.index() ]);
+        // is not OK in case this is a proxy. Hence
+        // we first have to copy the value. Notice that
+        // this is still not OK, if the vector type itself returns
+        // proxy references.
+        dblock rhsValue(d[i.index()]);
+        auto&& rhs = Impl::asVector(rhsValue);
+        for (coliterator j=(*i).begin(); j.index()<i.index(); ++j)
+          Impl::asMatrix(*j).mmv(Impl::asVector(v[j.index()]),rhs);
+        Impl::asVector(v[i.index()]) = rhs;           // Lii = I
+      }
+
+      // upper triangular solve
+      rowiterator rendi=A.beforeBegin();
+      for (rowiterator i=A.beforeEnd(); i!=rendi; --i)
+      {
+        // We need to be careful here: Directly using
+        // auto rhs = Impl::asVector(v[ i.index() ]);
+        // is not OK in case this is a proxy. Hence
+        // we first have to copy the value. Notice that
+        // this is still not OK, if the vector type itself returns
+        // proxy references.
+        vblock rhsValue(v[i.index()]);
+        auto&& rhs = Impl::asVector(rhsValue);
+        coliterator j;
+        for (j=(*i).beforeEnd(); j.index()>i.index(); --j)
+          Impl::asMatrix(*j).mmv(Impl::asVector(v[j.index()]),rhs);
+        auto&& vi = Impl::asVector(v[i.index()]);
+        Impl::asMatrix(*j).mv(rhs,vi);           // diagonal stores inverse!
+      }
+    }
+
+    // recursive function template to access first entry of a matrix
+    template<class M>
+    typename M::field_type& firstMatrixElement (M& A, typename std::enable_if_t<!Dune::IsNumber<M>::value>* sfinae = nullptr)
+    {
+      return firstMatrixElement(*(A.begin()->begin()));
+    }
+
+    template<class K>
+    K& firstMatrixElement (K& A, typename std::enable_if_t<Dune::IsNumber<K>::value>* sfinae = nullptr)
+    {
+      return A;
+    }
+
+    template<class K, int n, int m>
+    K& firstMatrixElement (FieldMatrix<K,n,m>& A)
+    {
+      return A[0][0];
+    }
+
+    /*! ILU decomposition of order n
+            Computes ILU decomposition of order n. The matrix ILU should
+        be an empty matrix in row_wise creation mode. This allows the user
+        to either specify the number of nonzero elements or to
+            determine it automatically at run-time.
+     */
+    template<class M>
+    void blockILUDecomposition (const M& A, int n, M& ILU)
+    {
+      // iterator types
+      typedef typename M::ColIterator coliterator;
+      typedef typename M::ConstRowIterator crowiterator;
+      typedef typename M::ConstColIterator ccoliterator;
+      typedef typename M::CreateIterator createiterator;
+      typedef typename M::field_type K;
+      typedef std::map<size_t, int> map;
+      typedef typename map::iterator mapiterator;
+
+      // symbolic factorization phase, store generation number in first matrix element
+      crowiterator endi=A.end();
+      createiterator ci=ILU.createbegin();
+      for (crowiterator i=A.begin(); i!=endi; ++i)
+      {
+        map rowpattern; // maps column index to generation
+
+        // initialize pattern with row of A
+        for (ccoliterator j=(*i).begin(); j!=(*i).end(); ++j)
+          rowpattern[j.index()] = 0;
+
+        // eliminate entries in row which are to the left of the diagonal
+        for (mapiterator ik=rowpattern.begin(); (*ik).first<i.index(); ++ik)
+        {
+          if ((*ik).second<n)
+          {
+            coliterator endk = ILU[(*ik).first].end();                       // end of row k
+            coliterator kj = ILU[(*ik).first].find((*ik).first);                       // diagonal in k
+            for (++kj; kj!=endk; ++kj)                       // row k eliminates in row i
+            {
+              // we misuse the storage to store an int. If the field_type is std::complex, we have to access the real/abs part
+              // starting from C++11, we can use std::abs to always return a real value, even if it is double/float
+              using std::abs;
+              int generation = (int) Simd::lane(0, abs( firstMatrixElement(*kj) ));
+              if (generation<n)
               {
-                rowpattern[kj.index()] = generation+1;
+                mapiterator ij = rowpattern.find(kj.index());
+                if (ij==rowpattern.end())
+                {
+                  rowpattern[kj.index()] = generation+1;
+                }
               }
             }
           }
         }
+
+        // create row
+        for (mapiterator ik=rowpattern.begin(); ik!=rowpattern.end(); ++ik)
+          ci.insert((*ik).first);
+        ++ci;           // now row i exist
+
+        // write generation index into entries
+        coliterator endILUij = ILU[i.index()].end();;
+        for (coliterator ILUij=ILU[i.index()].begin(); ILUij!=endILUij; ++ILUij)
+          Simd::lane(0, firstMatrixElement(*ILUij)) = (Simd::Scalar<K>) rowpattern[ILUij.index()];
       }
 
-      // create row
-      for (mapiterator ik=rowpattern.begin(); ik!=rowpattern.end(); ++ik)
-        ci.insert((*ik).first);
-      ++ci;           // now row i exist
-
-      // write generation index into entries
-      coliterator endILUij = ILU[i.index()].end();;
-      for (coliterator ILUij=ILU[i.index()].begin(); ILUij!=endILUij; ++ILUij)
-        Simd::lane(0,firstmatrixelement(*ILUij)) = (Simd::Scalar<K>) rowpattern[ILUij.index()];
-    }
-
-    // copy entries of A
-    for (crowiterator i=A.begin(); i!=endi; ++i)
-    {
-      coliterator ILUij;
-      coliterator endILUij = ILU[i.index()].end();;
-      for (ILUij=ILU[i.index()].begin(); ILUij!=endILUij; ++ILUij)
-        (*ILUij) = 0;           // clear row
-      ccoliterator Aij = (*i).begin();
-      ccoliterator endAij = (*i).end();
-      ILUij = ILU[i.index()].begin();
-      while (Aij!=endAij && ILUij!=endILUij)
+      // copy entries of A
+      for (crowiterator i=A.begin(); i!=endi; ++i)
       {
-        if (Aij.index()==ILUij.index())
+        coliterator ILUij;
+        coliterator endILUij = ILU[i.index()].end();;
+        for (ILUij=ILU[i.index()].begin(); ILUij!=endILUij; ++ILUij)
+          (*ILUij) = 0;           // clear row
+        ccoliterator Aij = (*i).begin();
+        ccoliterator endAij = (*i).end();
+        ILUij = ILU[i.index()].begin();
+        while (Aij!=endAij && ILUij!=endILUij)
         {
-          *ILUij = *Aij;
-          ++Aij; ++ILUij;
-        }
-        else
-        {
-          if (Aij.index()<ILUij.index())
-            ++Aij;
+          if (Aij.index()==ILUij.index())
+          {
+            *ILUij = *Aij;
+            ++Aij; ++ILUij;
+          }
           else
-            ++ILUij;
+          {
+            if (Aij.index()<ILUij.index())
+              ++Aij;
+            else
+              ++ILUij;
+          }
         }
       }
+
+      // call decomposition on pattern
+      blockILU0Decomposition(ILU);
     }
 
-    // call decomposition on pattern
-    bilu0_decomposition(ILU);
-  }
-
-  namespace ILU {
-
+    //! a simple compressed row storage matrix class
     template <class B, class Alloc = std::allocator<B>>
     struct CRS
     {
@@ -371,13 +369,17 @@ namespace Dune {
       }
     } // end convertToCRS
 
+    template<class CRS, class InvVector, class X, class Y>
+    DUNE_DEPRECATED_MSG("Will be removed after release 2.8. Use blockILUBacksolve.")
+    void bilu_backsolve (const CRS& lower, const CRS& upper, const InvVector& inv, X& v, const Y& d)
+    { blockILUBacksolve(lower, upper, inv, v, d); }
 
     //! LU backsolve with stored inverse in CRS format for lower and upper triangular
     template<class CRS, class InvVector, class X, class Y>
-    void bilu_backsolve (const CRS& lower,
-                         const CRS& upper,
-                         const InvVector& inv,
-                         X& v, const Y& d)
+    void blockILUBacksolve (const CRS& lower,
+                            const CRS& upper,
+                            const InvVector& inv,
+                            X& v, const Y& d)
     {
       // iterator types
       typedef typename Y :: block_type  dblock;
@@ -424,8 +426,23 @@ namespace Dune {
 
   } // end namespace ILU
 
-
   /** @} end documentation */
+
+  template<class M>
+  DUNE_DEPRECATED_MSG("Will be removed after release 2.8. Use ILU::blockILU0Decomposition.")
+  void bilu0_decomposition (M& A) { ILU::blockILU0Decomposition(A); }
+
+  template<class M, class X, class Y>
+  DUNE_DEPRECATED_MSG("Will be removed after release 2.8. Use ILU::blockILUBacksolve.")
+  void bilu_backsolve (const M& A, X& v, const Y& d) { ILU::blockILUBacksolve(A, v, d); }
+
+  template<class M>
+  DUNE_DEPRECATED_MSG("Will be removed after release 2.8. Use ILU::firstMatrixElement.")
+  decltype(auto) firstmatrixelement (M& A) { return ILU::firstMatrixElement(A); }
+
+  template<class M>
+  DUNE_DEPRECATED_MSG("Will be removed after release 2.8. Use ILU::blockILUDecomposition.")
+  void bilu_decomposition (const M& A, int n, M& ilu) { return ILU::blockILUDecomposition(A, n, ilu); }
 
 } // end namespace
 
