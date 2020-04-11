9ae31858d26c3b85b8cbc3c9bdd87bd1d36fa438
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-04-11 16:57:14 +0200

    Add FieldType a type trait for the field type of nested matrix/vector types

---
+++ dune/istl/fieldtype.hh
@@ -0,0 +1,96 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_ISTL_FIELDTYPE_HH
+#define DUNE_ISTL_FIELDTYPE_HH
+
+#include <algorithm>
+#include <type_traits>
+
+#include <dune/common/indices.hh>
+#include <dune/common/typetraits.hh>
+#include <dune/common/hybridutilities.hh>
+
+/*!
+ * \file
+ * \brief Helper alias to determine the field type of a nested block vector/matrix
+ */
+
+// forward declaration
+namespace Dune {
+template<typename... Args>
+class MultiTypeBlockVector;
+template<typename FirstRow, typename... Args>
+class MultiTypeBlockMatrix;
+} // end namespace Dune
+
+namespace Dune::Impl {
+
+// end of recursion
+template<typename T, bool isNumber = IsNumber<T>{}>
+struct FieldType
+{ using type = T; };
+
+// continue with T::field_type
+template<typename T>
+struct FieldType<T, false>
+{ using type = typename T::field_type; };
+
+template<typename... Args>
+struct FieldType<MultiTypeBlockVector<Args...>, false>
+{
+  using V = MultiTypeBlockVector<Args...>;
+  using Block0 = typename std::decay_t<decltype(std::get<0>(std::declval<V>()))>;
+  using FieldType0 = typename FieldType<Block0>::type;
+  static constexpr bool hasUniqueFieldType = []{
+    bool isSameType = true;
+    using namespace Dune::Hybrid;
+    forEach(integralRange(index_constant<sizeof...(Args)>()), [&](auto&& i) {
+      using FT = typename std::decay_t<decltype(std::get<i>(std::declval<V>()))>;
+      isSameType &= std::is_same_v<FieldType0, FT>;
+    });
+    return isSameType;
+  }();
+  // is there is no common type fall back to double
+  using type = std::conditional_t<hasUniqueFieldType, FieldType0, double>;
+};
+
+// special case: empty MultiTypeBlockVector
+template<>
+struct FieldType<MultiTypeBlockVector<>, false>
+{
+  using type = void;
+};
+
+template<typename FirstRow, typename... Args>
+struct FieldType<MultiTypeBlockMatrix<FirstRow, Args...>, false>
+{
+  using M = MultiTypeBlockMatrix<FirstRow, Args...>;
+  using Block00 = typename std::decay_t<decltype(std::get<0>(std::get<0>(std::declval<M>())))>;
+  using FieldType00 = typename FieldType<Block00>::type;
+  static constexpr bool hasUniqueFieldType = []{
+    bool isSameType = true;
+    using namespace Dune::Hybrid;
+    forEach(integralRange(index_constant<1+sizeof...(Args)>()), [&](auto&& i) {
+      using namespace Dune::Hybrid; // needed for icc, see issue #31
+      forEach(integralRange(index_constant<FirstRow::size()>()), [&](auto&& j) {
+        using FT = typename std::decay_t<decltype(std::get<j>(std::get<i>(std::declval<M>())))>;
+        isSameType &= std::is_same_v<FieldType00, FT>;
+      });
+    });
+    return isSameType;
+  }();
+  // is there is no common type fall back to double
+  using type = std::conditional_t<hasUniqueFieldType, FieldType00, double>;
+};
+
+} // end namespace Dune::Impl
+
+namespace Dune {
+
+template<typename T>
+using FieldType = typename Impl::FieldType<T>::type;
+
+} // end namespace Dune
+
+#endif
