fcaa4e6f7597c6b5be3f8cfd39318a0fcad3fad3
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-06 17:17:14 +0100

    Update package.py.

--- package.py
+++ package.py
@@ -1,13 +1,16 @@
 # Meta data
 name="dune-istl"
-version="2.8.201014"
+version="2.8"
 author="The Dune Core developers"
 author_email="dune@lists.dune-project.org"
-description="This is the iterative solver template library which provides generic sparse matrix/vector classes and a variety of solvers based on these classes. A special feature is the use of templates to exploit the recursive block structure of finite element matrices at compile time. Available solvers include Krylov methods, (block-) incomplete decompositions and aggregation-based algebraic multigrid."
+description="This is the iterative solver template library which provides generic sparse matrix/vector classes and a variety of solvers based on these classes."
 url="https://gitlab.dune-project.org/core/dune-istl"
 
+# DUNE dependencies
+dune_dependencies=['dune-common']
+
 # Package dependencies
-install_requires=['dune-common']
+install_requires=[]
 
 # Module libaries that have to be compiled (without the _ prefix)
 modules=['istl']
@@ -23,6 +26,5 @@ include config.h.cmake
 include dune-istl.pc.in
 include dune.module
 include LICENSE.md
-include pyproject.toml
 include README.md
 '''
