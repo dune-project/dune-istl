83663b57f7e3e481d704c6c18d0127f1eddb7583
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-24 11:07:06 +0100

    Replace setup.py by package.py.

--- package.py
+++ package.py
@@ -5,23 +5,7 @@ author="The Dune Core developers"
 author_email="dune@lists.dune-project.org"
 description="This is the iterative solver template library which provides generic sparse matrix/vector classes and a variety of solvers based on these classes."
 url="https://gitlab.dune-project.org/core/dune-istl"
+install_requires=[]
 
 # DUNE dependencies
 dune_dependencies=['dune-common']
-
-# Package dependencies
-install_requires=[]
-
-# Files to include in the source package
-manifest='''\
-graft cmake
-graft doc
-graft dune
-graft python
-include CMakeLists.txt
-include config.h.cmake
-include dune-istl.pc.in
-include dune.module
-include LICENSE.md
-include README.md
-'''
